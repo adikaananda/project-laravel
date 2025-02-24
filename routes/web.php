f3aac51d3b57d0023913fccc9a0d5a5d8be24ff1
Adika Ananda <kadekadikaananda2006@gmail.com.com>
2025-02-24 21:01:45 +0800

    create category controller

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin', DashboardController::class)->name("dashboard");
+Route::get('/admin', DashboardController::class){->name("dashboard");
+  Route::get('/', DashboardController::class)->name('dashboard');
 
 Route::get('login',  function () {
   return view('login.index');
